bfe000aeeac8fb141bc1b4b0bdfe970db2565c46
SandyFit <sandy07r@gmail.com>
2024-09-06 00:06:53 -0500

    Implemented Hero and Navbar animations

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../assets/css/borders.css';
 import MobileMenu from '../components/ui/MobileMenu';
 import NavItem from '../components/ui/NavItem';
 import MenuBtn from '../components/buttons/MenuBtn';
+import gsap from 'gsap';
+
 
 const Navbar = () => {
 
@@ -19,6 +21,40 @@ const Navbar = () => {
         setVisible(!visible);
     };
 
+    useEffect(() => {
+        const master = gsap.timeline();
+
+        // Split by words only, disable characters and lines
+        
+        const setInitialState = () => {
+            gsap.set(['.logo', '.nav__item', '.menu'], {
+                yPercent: -100,
+                autoAlpha: 0
+            });
+        };
+
+        const navAnimation = () => {
+            const tl = gsap.timeline({
+                defaults: {
+                    ease: 'power3.inOut',
+                    duration: 0.5,
+                    stagger: 0.1, // Stagger for smooth animation between words
+                }
+            });
+            tl.to(['.logo', '.nav__item', '.menu'], {
+                yPercent: 0,
+                autoAlpha: 1
+            });
+
+            return tl;
+        };
+
+        master
+            .add(setInitialState())
+            .add(navAnimation());
+
+    }, []);
+
     return (
         <section className='w-full h-[80px] absolute top-0 z-20 flex justify-between px-6 md:px-10
             base:px-12 xl-sm:px-16'>
@@ -26,20 +62,31 @@ const Navbar = () => {
                 <div className="grid grid-cols-12 place-content-between items-center w-full relative 
                     before:absolute before:h-[1px] before:bg-cyan-300 before:w-full before:top-12 
                     before:lg:top-[3.6rem] before:xl:top-16">
-                    <h3 className='xsm-title col-span-6 xl:col-span-3 col-start-1 row-start-1'>
+                    <h3 className='xsm-title col-span-6 xl:col-span-3 col-start-1 row-start-1 logo'>
                         Trish Ramos
                     </h3>
 
                     <div className='hidden lg-lg:flex col-span-5 col-start-9 w-full relative justify-end'>
-                        <ul className="flex justify-between gap-2 xsm-title">
-                            <NavItem to="projects">projects,</NavItem>
-                            <NavItem to="info">info,</NavItem>
-                            <NavItem to="contact">contact,</NavItem>
-                            <NavItem to="cv">CV</NavItem>
+                        <ul className="flex justify-between gap-2 xsm-title ">
+                            <span className='nav__item'>
+                                <NavItem to="projects">projects,</NavItem>
+                            </span>
+                            <span className='nav__item'>
+                                <NavItem to="info">info,</NavItem>
+                            </span>
+                            <span className='nav__item'>
+                                <NavItem to="contact">contact,</NavItem>
+                            </span>
+                            <span className='nav__item'>
+                                <NavItem to="cv">CV</NavItem>
+                            </span>
+                            
+                            
+                            
                         </ul>
                     </div>
 
-                    <div className="flex lg-lg:hidden col-span-1 col-start-11 row-start-1 ">
+                    <div className="flex lg-lg:hidden col-span-1 col-start-11 row-start-1 nav__list">
                         <MenuBtn onClick={handleClick} visible={visible} />
                         <div className="flex">
                             <MobileMenu isMenuOpen={visible} />
--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -9,49 +9,103 @@ import ColorBtn from '../components/buttons/ColorBtn';
 
 
 const Hero = () => {
+
     useEffect(() => {
         gsap.registerPlugin(ScrollTrigger);
 
-        const initialTimeline = gsap.timeline({ defaults: { ease: "power3.out", duration: 2.5, delay: 0.5 } });
-        initialTimeline.fromTo('.small-title', { opacity: 0 }, { opacity: 1, stagger: 0.5, delay: 1.5 });
-        initialTimeline.fromTo('.turning-text', { rotateY: '0deg' }, { rotateY: '360deg', ease: "power3.inOut" });
+        const master = gsap.timeline();
+        const heroTitle = new SplitType('.hero-title', { types: 'chars' });
+        const chars = heroTitle.chars;
 
-        const overlayTimeline = gsap.timeline();
-        overlayTimeline.to('.overlay-first', { duration: 1, left: '-100%', ease: "power3.inOut" });
-        overlayTimeline.to('.overlay-second', { duration: 1, left: '-100%', ease: "power3.inOut", immediateRender: false }, "-=0.8");
+        const setInitialState = () => {
+            
+            gsap.set(['.description', '.color__switch'], {
+                autoAlpha: 0
+            })
 
-        const scrollTimeline = gsap.timeline({
-            scrollTrigger: {
+            gsap.set(chars, {
+                y: 100,
+                autoAlpha: 0
+            });
+
+            gsap.set(['.small__text', '.small__title' ], {
+                yPercent: 100,
+                autoAlpha: 0
+            });
+        };
+
+        const titleAnimation = () => {
+            const tl = gsap.timeline({
+                defaults: {
+                    ease: "power3.out",
+
+                }
+            });
+
+            tl.to(chars, {
+                y: 0,
+                autoAlpha: 1,
+                stagger: 0.1,
+                duration: 1,
+            })
+                .to('.small__text', {
+                    yPercent: 0,
+                    autoAlpha: 1,
+                    stagger: 0.5,
+                    
+                }, .5)
+            
+                .to('.small__title', {
+                    yPercent: 0,
+                    autoAlpha: 1,
+                    stagger: 0.1,
+                    delay: 1
+                }, '<')
+
+                .to(['.description', '.color__switch'], {
+                    autoAlpha: 1,
+                    stagger: 0.1,
+                    delay: .2
+                }, '<');
+            
+            return tl;
+        }
+
+        const scrollTimeline = () => {
+            const st = gsap.timeline({
+                scrollTrigger: {
                 trigger: "#index",
                 start: "top top",
                 end: "bottom top",
                 scrub: true,
                 toggleActions: 'play none none reverse',
-            }
-        });
-        scrollTimeline.to('.hero-content', { opacity: 0, duration: 0.5 });
-
-        const heroTitle = new SplitType('.hero-title', { types: 'chars' });
-        const chars = heroTitle.chars;
-        gsap.fromTo(chars, { y: 100, opacity: 0 }, { y: 0, opacity: 1, stagger: 0.2, duration: 2, ease: 'power4.out' });
-
-        return () => {
-            initialTimeline.kill();
-            overlayTimeline.kill();
-            scrollTimeline.kill();
-        };
+                }
+            });
+                 
+            st.to('.hero-content', {
+                autoAlpha: 0,
+                duration: 0.5
+            });
+            return st;
+       }
+        master 
+            .add(setInitialState())
+            .add(titleAnimation(), '<')
+            .add(scrollTimeline(), '<')
+        
     }, []);
 
     return (
         <section id="index"
-            className='w-full min-h-screen flex flex-col bg-zinc-950 relative px-6 md:px-10 base:px-12 xl-sm:px-16'>
+            className='w-full min-h-screen flex flex-col bg-zinc-950 relative px-6 md:px-10 base:px-12 
+                xl-sm:px-16 hero-content'>
 
             <div border-cut="bottom-right-black" className="grid w-full">
                 <header className="w-full flex flex-col mt-5 lg:mt-[8rem] relative flex-grow">
                     <div className="w-full grid grid-cols-1 lg:grid-cols-4 lg-xl:grid-cols-5 2xl-sm:grid-cols-8 
                         2xl-xl:grid-cols-12">
                         <article className="w-full col-span-1 lg:col-span-2 lg-xl:col-span-2 xl:col-span-2 
-                            2xl-sm:col-span-3 row-start-2 col-start-1 flex flex-col  mt-24
+                            2xl-sm:col-span-3 row-start-2 col-start-1 flex flex-col  mt-24 description
                             lg:mt-[-1.2rem] lg-lg:mt-[-2.6rem] xl:mt-[-1.8rem] relative">
                             <p className='w-full text-xxsmall text-justify'>
                                 Designer & Developer based in Colombia. Currently seeking dev roles to
@@ -68,7 +122,8 @@ const Hero = () => {
                             </p>
                         </article>
 
-                        <aside className="w-full hidden lg-lg:flex col-span-2 col-start-11 ml-[5.4rem] items-center ">
+                        <aside className="w-full hidden lg-lg:flex col-span-2 col-start-11 ml-[5.4rem] items-center 
+                            color__switch">
                             <p className='text-[.8rem] font-100 text-zinc-600 tracking-widest uppercase'>
                                 color shift
                             </p>
@@ -81,13 +136,13 @@ const Hero = () => {
                         2xl-lg:mt-32 2xl-xl:mt-28 3xl:mt-32">
                         <div className='w-full hidden lg:grid grid-cols-1 xl:grid-cols-12 '>
                             <h3 className='title-h3 col-span-6 col-start-1 lg:col-start-9 xl:col-start-8 row-start-1 
-                                whitespace-nowrap flex justify-end 3xl:mr-6'>
+                                whitespace-nowrap flex justify-end 3xl:mr-6 small__title'>
                                 Graphic
                                 <span className='title-h3-thin mx-3 '>designer</span>
                                 &
                             </h3>
                             <h1 className="big-title col-span-12 col-start-1 row-start-2 lg-lg:mt-3 xl-sm:mt-5 xl:mt-10 
-                                2xl-sm:mt-12 2xl-md:mt-[3.2rem] 2xl-xl:mt-[3.8rem] text-center whitespace-nowrap">
+                                2xl-sm:mt-12 2xl-md:mt-[3.2rem] 2xl-xl:mt-[3.8rem] text-center whitespace-nowrap hero-title">
                                 frontend developer
                             </h1>
                         </div>
@@ -109,7 +164,7 @@ const Hero = () => {
             <footer className="mt-auto w-full mb-6">
                 <div className="grid grid-cols-1 lg:grid-cols-3 w-full lg-md:place-content-between whitespace-nowrap">
                     <div className="hidden lg-lg:flex text-xxsmall-cyan col-span-1 col-start-1 row-start-1 
-                        justify-start items-center">
+                        justify-start items-center small__text">
                         portfolio
                         <span className='text-xxsmall-white ml-1.5'>
                             &copy;{new Date().getFullYear()}
@@ -117,15 +172,15 @@ const Hero = () => {
                     </div>
 
                     <div className="flex lg-lg:hidden text-xxsmall-white col-span-1 col-start-1 row-start-1 w-full 
-                        whitespace-nowrap justify-center lg:justify-start ">
+                        whitespace-nowrap justify-center lg:justify-start small__text">
                         folio &copy; {new Date().getFullYear()}
                     </div>
                     <div className="text-xxsmall-cyan col-span-1 col-start-1 lg:col-start-2 row-start-3 lg:row-start-1 
-                        mt-16 lg:mt-0 mb-2 lg:mb-0 flex justify-center items-center">
+                        mt-16 lg:mt-0 mb-2 lg:mb-0 flex justify-center items-center small__text">
                         [ &nbsp; &nbsp;<ScrollBtn /> &nbsp;]
                     </div>
                     <div className="col-span-1 col-start-1 lg:col-start-3 row-start-2 lg:row-start-1 flex items-center 
-                        justify-center lg:justify-end text-xxsmall">
+                        justify-center lg:justify-end text-xxsmall small__text">
                         <TimeZone/>
                     </div>
                 </div>
